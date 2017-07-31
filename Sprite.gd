4fa47ec25dbb49910659a716d451878200f24bb7
thanseehabdulla <thanseehabdulla@gmail.com>
2017-07-31 14:56:32 +0530

    pong game added

---
+++ Sprite.gd
@@ -0,0 +1,14 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
+func _on_Button_pressed():
+	get_node("Container/Label").set_text("we have changed the letter")
